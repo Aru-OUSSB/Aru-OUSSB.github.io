98ffe711a7bb7400dd5386c1704f2503b402a82f
Aru-OUSSB <ricky4work26@gmail.com>
2024-12-08 21:50:58 +0900

    Add files via upload

--- auto_update.jl
+++ auto_update.jl
@@ -1,32 +1,31 @@
 using Dates
 
-try
-    include("./mate_ID.jl")
-    f = open("./mate_ID.jl")
-    lines = readlines(f)
-    close(f)
-catch
-    include("./mate_ID_example.jl")
-    f = open("./mate_ID_example.jl")
-    lines = readlines(f)
-    close(f)
-    
-finally
-run(`git pull -f https://github.com/Aru-OUSSB/Aru-OUSSB.github.io.git main:main`)
+include("./mate_ID.jl")
+f = open("./mate_ID.jl")
+lines = readlines(f)
+close(f)
+
+# run(`git pull -f https://github.com/Aru-OUSSB/Aru-OUSSB.github.io.git main:main`)
 sleep(1)
 
-f = open("mate_ID.jl","w")
-for line in lines
-    println(f,line)
-end
+f = open("./mate_ID.jl")
+lines2 = readlines(f)
 close(f)
+if lines != lines2
+    f = open("./mate_ID.jl","w")
+    for line in lines
+        println(f,line)
+    end
+    close(f)
+    run(`git add mate_ID.jl`)
+    run(`git commit -m "change_ID"`)
+    run(`git push origin main`)
 end
-
 
 function run_update()
     while true
         # generate_static_html.jlを実行
-        run(`julia generate_static_html.jl`)
+        include("generate_static_html.jl")
             
         current_time = Dates.format(now(), "HH:MM")
         println("$(current_time): ランキングを更新しました")
