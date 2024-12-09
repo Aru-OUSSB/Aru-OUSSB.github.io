2ed29d941203d7b1fd6c3b9de6cd634ad15b2dd8
Aru-OUSSB <ricky4work26@gmail.com>
2024-12-09 11:50:53 +0900

    Update auto_update.jl

--- auto_update.jl
+++ auto_update.jl
@@ -13,7 +13,7 @@ run(`git add mate_ID.jl`)
 run(`git add mate.jl`)
 run(`git add README.md`)
 run(`git add start_auto_update.bat`)
-run(`git add .guthub`)
+run(`git add ./.guthub/workflows/deploy.yml`)
 
 try
     run(`git commit -m "shokisettei"`)
