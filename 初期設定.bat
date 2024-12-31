cc78e6ddd064dfe0b1a48100cab7d574c8c014dd
Aru-OUSSB <ricky4work26@gmail.com>
2024-12-31 16:29:40 +0900

    初期設定修正

--- 初期設定.bat
+++ 初期設定.bat
@@ -1,4 +1,5 @@
 git init
+git add 初期設定.bat
 git pull https://github.com/Aru-OUSSB/Aru-OUSSB.github.io.git main:main
 git checkout main
 git remote add origin https://github.com/Aru-OUSSB/Aru-OUSSB.github.io.git
