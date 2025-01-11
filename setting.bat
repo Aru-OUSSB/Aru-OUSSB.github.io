b3ce676a372ad9b129ceb252a7ea30d93dad1c91
Aru-OUSSB <ricky4work26@gmail.com>
2025-01-11 11:03:27 +0900

    Add files via upload

---
+++ setting.bat
@@ -0,0 +1,8 @@
+git init
+git config user.name "example"
+git config user.email "example@example.com"
+git add setting.bat
+git commit -m "setting"
+git pull https://github.com/Aru-OUSSB/Aru-OUSSB.github.io.git main:main
+git checkout main
+git remote add origin https://github.com/Aru-OUSSB/Aru-OUSSB.github.io.git
