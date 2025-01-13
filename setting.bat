b583a12553a1584e99e1c435a79c65237ff93a61
Riki Murakami <130717180+Ricky-Murakami@users.noreply.github.com>
2025-01-13 20:20:36 +0900

    Update setting.bat

--- setting.bat
+++ setting.bat
@@ -1,6 +1,6 @@
+git init
 git config user.name "example"
 git config user.email "example@example.com"
-git init
 git add setting.bat
 git commit -m "setting"
 git pull https://github.com/Aru-OUSSB/Aru-OUSSB.github.io.git main:main
